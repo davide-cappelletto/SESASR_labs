35915422ae96b1604ed24fd6e67c57a819ded925
PeppePercival <giuseppedeninarivera@gmail.com>
2024-01-19 18:20:29 +0100

    ended filter pipeline. Starting tuning process

--- src/localization_project/localization_project/recorder_alt.py
+++ src/localization_project/localization_project/recorder_alt.py
@@ -1,3 +1,4 @@
+#this file takes as input the topics' information coming from ekf simulation bag, and saves it into numpy files
 import rclpy
 from rclpy.node import Node
 import tf_transformations
@@ -15,11 +16,11 @@ class RecorderAltNode(Node):
         
         # Create the subscribers and synchronize the messages
         self.odom_sub = message_filters.Subscriber(self, Odometry, "/diff_drive_controller/odom")
-        #self.filter_sub = message_filters.Subscriber(self, Odometry, "/ekf")
+        self.filter_sub = message_filters.Subscriber(self, Odometry, "/ekf")
         self.ground_truth_sub = message_filters.Subscriber(self, Odometry, "/ground_truth")
         self.ts = message_filters.ApproximateTimeSynchronizer(
-            #[self.odom_sub, self.filter_sub, self.ground_truth_sub], 10, 0.02
-            [self.odom_sub, self.ground_truth_sub], 10, 0.02
+            [self.odom_sub, self.filter_sub, self.ground_truth_sub], 10, 0.02
+            
         )
         self.ts.registerCallback(self.store_data)
 
@@ -34,33 +35,23 @@ class RecorderAltNode(Node):
         yaw = math.atan2(t3, t4)
         return yaw 
 
-    #def store_data(self, odom, filter, ground_truth):
-    def store_data(self, odom, ground_truth):
+    def store_data(self, odom, filter, ground_truth):
         self.get_logger().info(
             f"Storing data... Current lenght {len(self.odom)}", throttle_duration_sec=5.0
         )
         self.odom.append([odom.pose.pose.position.x, odom.pose.pose.position.y, self.yaw_calc(odom.pose.pose.orientation)])
-        #self.filter.append([filter.pose.pose.position.x, filter.pose.pose.position.y, filter.pose.pose.orientation.z])
+        self.filter.append([filter.pose.pose.position.x, filter.pose.pose.position.y, filter.pose.pose.orientation.z])
         self.ground_truth.append([ground_truth.pose.pose.position.x, ground_truth.pose.pose.position.y, self.yaw_calc(ground_truth.pose.pose.orientation)])
-        # Convert incoming data to NumPy arrays
-        #odom_array = np.array([odom.pose.pose.position.x, odom.pose.pose.position.y, self.yaw_calc(odom.pose.pose.orientation)])
-        #filter_array = np.array([filter.pose.pose.position.x, filter.pose.pose.position.y, filter.pose.pose.orientation.z])
-        #ground_truth_array = np.array([ground_truth.pose.pose.position.x, ground_truth.pose.pose.position.y, self.yaw_calc(ground_truth.pose.pose.orientation)])
-
-        # Append data to existing arrays
-        #self.odom = np.vstack([self.odom, odom_array]) if self.odom else odom_array
-        #self.filter = np.vstack([self.filter, filter_array]) if self.filter else filter_array
-        #self.ground_truth = np.vstack([self.ground_truth, ground_truth_array]) if self.ground_truth else ground_truth_array
         
         
     def save_data(self):
         self.get_logger().info('Saving the data...')
         odom_path = Path('src/localization_project/stored_data/odom.npy').resolve()
-        #filter_path = Path('src/localization_project/stored_data/filter.npy').resolve()
+        filter_path = Path('src/localization_project/stored_data/filter.npy').resolve()
         gt_path = Path('src/localization_project/stored_data/ground_truth.npy').resolve()
 
         np.save(odom_path, np.array(self.odom))
-        #np.save(filter_path, np.array(self.filter))
+        np.save(filter_path, np.array(self.filter))
         np.save(gt_path, np.array(self.ground_truth))
 
     
@@ -73,9 +64,6 @@ def main(args=None):
         pass
     finally:
         node.save_data()
-        print(node.odom)
-        #print(node.filter)
-        print(node.ground_truth)
         rclpy.try_shutdown()
 
 
