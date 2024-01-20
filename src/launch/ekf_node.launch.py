982c1c7ecc63bd029d0c35460d7f44693263be13
PeppePercival <giuseppedeninarivera@gmail.com>
2024-01-20 17:01:48 +0100

    spostata launch in src

---
+++ src/launch/ekf_node.launch.py
@@ -0,0 +1,56 @@
+from launch.launch_description import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import (
+    ExecuteProcess,
+    RegisterEventHandler,
+    DeclareLaunchArgument,
+    EmitEvent,
+    TimerAction,
+    IncludeLaunchDescription
+)
+from launch.event_handlers import OnProcessExit, OnProcessStart
+from launch.events import Shutdown
+from launch.conditions import IfCondition
+
+from launch.substitutions import (
+    ThisLaunchFileDir,
+    PathJoinSubstitution,
+    LaunchConfiguration,
+    PythonExpression,
+)
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python import get_package_share_directory
+
+def generate_launch_description():
+    use_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Whether to use simulation time'
+    )
+
+    localization_project_pkg_share = get_package_share_directory('localization_project')
+
+    config = PathJoinSubstitution([FindPackageShare("localization_project"),"config","ekf_node.yaml"])
+    ekf_node = Node(
+        package="localization_project",
+        executable= "EKF_node",
+        output="screen",
+        #parameters=[config],
+        parameters=[{'config_file': PathJoinSubstitution([localization_project_pkg_share, 'config', 'ekf_node.yaml'])}
+        ]
+        )
+    
+    rosbag_output_dir = DeclareLaunchArgument(
+        'rosbag_output_dir',
+        default_value='localization_project/rosbags',
+        description='Directory where the rosbag should be saved'
+    )
+
+    rosbag_record_cmd = ExecuteProcess(
+        cmd = ["ros2", "bag", "record", "-o", "data", 
+               "/diff_drive_controller/odom", "/ground_truth", "/ekf" ],
+        cwd = PathJoinSubstitution([ThisLaunchFileDir(), '../../', LaunchConfiguration('rosbag_output_dir')]),
+        output = "screen",       
+    )
+
+    return LaunchDescription([use_sim_time_arg, ekf_node, rosbag_output_dir, rosbag_record_cmd])
